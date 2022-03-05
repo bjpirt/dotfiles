ec1ed7bc1d4df28287480f115ee7d06399485437
Ben Pirt <ben@pirt.co.uk>
2022-03-05 17:59:07 +0000

    Add vscode settings

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 PWD=`pwd`
-for file in `ls -a ./files/`;
+for file in `ls -a ./dotfiles/`;
 do
   if [ "$file" != "." ] && [ "$file" != ".." ]; then
-    fileloc="$PWD/files/$file"
+    fileloc="$PWD/dotfiles/$file"
     cmd="ln -sfF $fileloc $HOME/."
     echo $cmd
     $cmd
   fi
 done
+echo $(dirname "$0")
+
+ln -sf "$PWD/settings/vscode/settings.json" "$HOME/Library/Application Support/Code/User/"
+
